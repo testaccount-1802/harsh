0377fb35e3549c91bb48de89819bdf67538e759a
testaccount-1802 <246564852+testaccount-1802@users.noreply.github.com>
2026-03-18 17:59:08 +0800

    Add files via upload

--- SailorPieceCONFIGS.lua
+++ SailorPieceCONFIGS.lua
@@ -531,7 +531,7 @@ return function(ctx)
 
     Configs.ModuleDefinitions = {
         { key = "Home", label = "Home", tab = "Home", config = Configs.HomeConfig },
-        { key = "Sailor Piece", label = "Sailor Piece," tab = "Sailor Piece", config = Configs.SailorConfig },
+        { key = "Sailor Piece", label = "Sailor Piece", tab = "Sailor Piece", config = Configs.SailorConfig },
         { key = "Webhooks", label = "Webhooks", tab = "Home", config = Configs.WebhookConfig },
         { key = "Teleports", label = "Teleports", tab = "Main", config = Configs.TeleportsConfig },
         { key = "AutoPrompt", label = "Auto Prompt", tab = "Main", config = Configs.AutoPromptConfig },
